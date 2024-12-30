bf9b1e45cda461697ada86c12b815db202372dcc
jinpei0908 <jinpei0908@gmail.com>
2024-12-31 00:30:39 +0900

    feat: スクリプト修正

--- insert_date.bash
+++ insert_date.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-today=$(date '+%Y年%m月%d日')
-sed -I '' -e "3s/^更新日: .*/更新日: ${today}/" README.md
+if [[ $(git diff --exit-code README.md) == 1 ]]; then
+    today=$(date '+%Y年%m月%d日')
+    sed -I '' -e "4s/^更新日: .*/更新日: ${today}/" README.md
+fi
